4b3722469b90eb490038214108f7097c713e1095
George Higbie <gehigbie@gmail.com>
2023-07-04 17:43:12 -0500

    Add fetch to owner app

--- src/pages/toppings/edit-toppings.tsx
+++ src/pages/toppings/edit-toppings.tsx
@@ -1,16 +1,33 @@
 import AppLayout from '@/components/AppLayout';
 import PageTitle from '@/components/PageTitle';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 import { useRecoilState } from 'recoil';
-import { toppingsState } from 'state/states';
+import { initialDataLoad, pizzasState, toppingsState } from 'state/states';
 import styles from './edit-toppings.module.scss';
 import ViewToppingsCard from '@/components/toppings/ViewToppingsCard';
 import AddToppingsCard from '@/components/toppings/AddToppingsCard';
 import axios from 'axios';
 
 const EditToppings = () => {
+  const [pizzaList, setPizzaList] = useRecoilState(pizzasState);
   const [toppingsList, setToppingsList] = useRecoilState(toppingsState);
+  const [hasInitialDataLoaded, setInitialDataLoaded] =
+    useRecoilState(initialDataLoad);
+
+  const fetchInitialAppData = async () => {
+    if (!hasInitialDataLoaded) {
+      const pizzaResponse = await axios.get('/api/v1/pizza/pizzas');
+      const toppingsResponse = await axios.get('/api/v1/toppings/toppings');
+      setPizzaList(pizzaResponse.data.pizzasList);
+      setToppingsList(toppingsResponse.data.toppingsList);
+      setInitialDataLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchInitialAppData();
+  }, []);
 
   return (
     <AppLayout>
